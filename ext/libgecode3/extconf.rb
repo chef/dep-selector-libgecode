9e25df5e7162779e73448932017dc3fabd41c3cf
danielsdeleo <dan@getchef.com>
2014-04-05 10:20:37 -0700

    update build to handle pwd weirdness with __FILE__
    Also quiet build output.

--- ext/libgecode3/extconf.rb
+++ ext/libgecode3/extconf.rb
@@ -1,25 +1,56 @@
 require 'pp'
 
-gecode_vendor_dir = File.expand_path("../vendor/gecode-3.7.3", __FILE__)
-prefix = File.expand_path("../../../lib/dep-selector-libgecode3/vendor", __FILE__)
+module GecodeBuild
+  class BuildError < StandardError; end
 
-pp :prefix => prefix
+  GECODE_VENDOR_DIR = File.expand_path("../vendor/gecode-3.7.3", __FILE__).freeze
 
-configure_cmd = %W[
-  ./configure
-  --prefix=#{prefix}
-  --disable-doc-dot
-  --disable-doc-search
-  --disable-doc-tagfile
-  --disable-doc-chm
-  --disable-doc-docset
-  --disable-qt
-  --disable-examples
-  --disable-flatzinc
-]
+  PREFIX = File.expand_path("../../../lib/dep-selector-libgecode3/vendored-gecode", __FILE__).freeze
 
-Dir.chdir(gecode_vendor_dir) do
-  system(configure_cmd) &&
-    system("make -j 5")
+  LOG_FILE_PATH = File.expand_path("../gecode_build.log", __FILE__)
+
+  def self.gecode_vendor_dir
+    GECODE_VENDOR_DIR
+  end
+
+  def self.prefix
+    PREFIX
+  end
+
+  def self.configure_cmd
+    %W[
+      ./configure
+      --prefix=#{prefix}
+      --disable-doc-dot
+      --disable-doc-search
+      --disable-doc-tagfile
+      --disable-doc-chm
+      --disable-doc-docset
+      --disable-qt
+      --disable-examples
+      --disable-flatzinc
+    ]
+  end
+
+  def self.system(*args)
+    print("-> #{args.join(' ')}\n")
+    super(*args, [:out, :err] => [LOG_FILE_PATH, "w"])
+  end
+
+  def self.run_build_commands
+    system(*configure_cmd) &&
+      system("make", "clean") &&
+      system("make", "-j", "5") &&
+      system("make", "install")
+  end
+
+  def self.run
+    Dir.chdir(gecode_vendor_dir) do
+      run_build_commands or raise BuildError, "Failed to build gecode library. See log for details: #{LOG_FILE_PATH}"
+    end
+  end
+
 end
+
+GecodeBuild.run
 
