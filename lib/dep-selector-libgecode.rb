3001fa1bf14544dea99b2df5a751a35aa43dcff1
danielsdeleo <dan@getchef.com>
2014-04-06 18:42:11 -0700

    Add path helper functions

--- lib/dep-selector-libgecode.rb
+++ lib/dep-selector-libgecode.rb
@@ -1,4 +1,15 @@
 require "dep-selector-libgecode/version"
 
 module DepSelectorLibgecode
+
+  VENDORED_GECODE_DIR = File.expand_path("../dep-selector-libgecode/vendored-gecode", __FILE__)
+
+  def self.opt_path
+    File.join(VENDORED_GECODE_DIR, "lib")
+  end
+
+  def self.include_path
+    File.join(VENDORED_GECODE_DIR, "include")
+  end
 end
+
--- lib/dep-selector-libgecode/version.rb
+++ lib/dep-selector-libgecode/version.rb
@@ -1,3 +1,3 @@
 module DepSelectorLibgecode
-  VERSION = "0.0.1"
+  VERSION = "1.0.0.alpha.0"
 end
